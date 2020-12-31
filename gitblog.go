22e1372a79b8e0d54d8203c88ae16a94160a56b8
Joe McCormick <31295332+iamjoemccormick@users.noreply.github.com>
2020-12-30 18:43:08 -0600

    Implemented the ability to return a default file when a directory is specified as the URL. readFile now works if a supported file ending was provided in the URL.

--- gitblog.go
+++ gitblog.go
@@ -6,33 +6,45 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strings"
 
 	"github.com/gomarkdown/markdown"
 )
 
 var (
-	staticBase         string = "static/"
-	supportedFileTypes        = [3]string{".md", ".html", ".htm"}
+	staticBase            string = "static/"
+	supportedFileTypes           = [3]string{".md", ".html", ".htm"} // In order of greatest to least priority in case of duplicates.
+	supportedDefaultFiles        = [2]string{"index", "home"}        // In order of greatest to least priority in case of duplicates.
 )
 
 func main() {
 	log.SetOutput(os.Stdout)
-	http.HandleFunc("/", urlHandler)
+	http.HandleFunc("/", handleURL)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
-func urlHandler(w http.ResponseWriter, r *http.Request) {
+func handleURL(w http.ResponseWriter, r *http.Request) {
 
-	requestedPath := r.URL.Path[1:]
-	ft, content, err := readFile(requestedPath)
+	path := r.URL.Path[1:]
+	var (
+		ft      string
+		content []byte
+		err     error
+	)
+
+	if path == "" || strings.HasSuffix(path, "/") {
+		ft, content, err = handleDirectory(path)
+	} else {
+		ft, content, err = readFile(path)
+	}
 
 	if err != nil {
 		//TODO: Better handling of 404 and internal server errors include returning proper HTTP response codes.
 
 		if os.IsNotExist(err) {
-			fmt.Fprintf(w, "404: Page '%s' does not exist!", requestedPath)
+			fmt.Fprintf(w, "404: Page '%s' does not exist!", path)
 		} else {
-			fmt.Fprintf(w, "500: Internal server error attempting to access '%s'.", requestedPath)
+			fmt.Fprintf(w, "500: Internal server error attempting to access '%s'.", path)
 		}
 	} else {
 
@@ -45,16 +57,43 @@ func urlHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 }
+
+// handleDirectory checks if there is a supportedDefaultFile in a given directory path and returns the result of readFile if it exists, or an error.
+func handleDirectory(path string) (fileType string, content []byte, err error) {
+
+	for _, d := range supportedDefaultFiles {
+		ft, content, err := readFile(path + d)
+
+		if err != nil {
+			continue
+		}
+		return ft, content, err
+	}
+
+	//TODO: Consider automatically generating a default page if one was not specified.
+
+	log.Printf("No file found under '%s' with the following names %s and file types %s", path, supportedDefaultFiles, supportedFileTypes)
+	return "", nil, err
+}
+
+// readFile takes a path that may or may not end in a filename extension.
+// If the path does not end in a supportedFileType it will check if there is a file with any of the supportedFileTypes at that path.
+// If the path ends in a supportedFileType but no file exists for that type, an error will be returned.
 func readFile(path string) (fileType string, content []byte, err error) {
 
 	for _, t := range supportedFileTypes {
-		content, err = ioutil.ReadFile(staticBase + path + t)
+
+		if strings.HasSuffix(path, t) {
+			content, err = ioutil.ReadFile(staticBase + path)
+		} else {
+			content, err = ioutil.ReadFile(staticBase + path + t)
+		}
 
 		if err != nil && os.IsNotExist(err) {
-			log.Printf("Did not find a file named '%s'", staticBase+path+t)
+			log.Printf("Did not find a file at '%s' with supported file type '%s'", staticBase+path, t)
 			continue
 		} else if err != nil {
-			log.Printf("Unknown error attempting to read file '%s': %s", path, err)
+			log.Printf("Unknown error attempting to read file '%s' with ending '%s': %s", staticBase+path, t, err)
 			return "", nil, err
 		} else {
 			return t, content, nil
